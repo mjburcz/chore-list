6cf21825f12b5bf1eb56f1a689cfd871d2d20520
mjburcz <malloryjaneburcz@gmail.com>
2021-10-10 13:55:50 -0400

    Now actually including bootstrap

--- src/components/ChoreList.js
+++ src/components/ChoreList.js
@@ -1,30 +1,40 @@
 import React from "react";
 import Chore from "./Chore";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function ChoreList(props) {
   return (
     <>
-      <h2>To Do</h2>
-      {props.choreList.map((chore) => {
-        return (
-          <ul key={chore.id}>
-            {!chore.complete && (
-              <Chore chore={chore} handleToggle={props.handleToggle} />
-            )}
-          </ul>
-        );
-      })}
-
-      <h2>Completed</h2>
-      {props.choreList.map((chore) => {
-        return (
-          <ul key={chore.id}>
-            {chore.complete && (
-              <Chore chore={chore} handleToggle={props.handleToggle} />
-            )}
-          </ul>
-        );
-      })}
+      <Container>
+        <Row>
+          <Col>
+            <h2>To Do</h2>
+            {props.choreList.map((chore) => {
+              return (
+                <ul key={chore.id}>
+                  {!chore.complete && (
+                    <Chore chore={chore} handleToggle={props.handleToggle} />
+                  )}
+                </ul>
+              );
+            })}
+          </Col>
+          <Col>
+            <h2>Completed</h2>
+            {props.choreList.map((chore) => {
+              return (
+                <ul key={chore.id}>
+                  {chore.complete && (
+                    <Chore chore={chore} handleToggle={props.handleToggle} />
+                  )}
+                </ul>
+              );
+            })}
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
